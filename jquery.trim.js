e3bdb02e580cd50811d9094d13dd123224377acb
John Antoni Griffiths <john@johnantoni.com>
2010-03-12 22:14:28 +0000

    init

---
+++ jquery.trim.js
@@ -0,0 +1,15 @@
+﻿/*
+ * super-fast javascript trim based on http://blog.stevenlevithan.com/archives/faster-trim-javascript
+ */
+
+(function($){
+    
+    $.fn.trimVal = function() {
+	    var	str = this.val().replace(/^\s\s*/, ''),
+		ws = /\s/,
+		i = str.length;
+	    while (ws.test(str.charAt(--i)));
+	    return str.slice(0, i + 1);
+    };
+    
+})(jQuery);
