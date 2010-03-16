9a5df99a7a91b9b49396c65d7d416479f4764c83
John Antoni Griffiths <john@johnantoni.com>
2010-03-16 00:19:23 +0000

    fix

--- jquery.trim.js
+++ jquery.trim.js
@@ -2,18 +2,18 @@
  * super-fast javascript trim based on http://blog.stevenlevithan.com/archives/faster-trim-javascript
  */
 
-// jQuery extension to get the value, trim and return it (often useful for forms)
-(function($){
-  $.fn.trimVal = function() {
-    return this.val().trimFast();
-  };
-})(jQuery);
-
 // prototype in our new trim method (named differently to not offend others relying on native version)
-String.prototype.trimFast = function() {
+String.prototype.trimfast = function() {
 	var	str = this.replace(/^\s\s*/, ''),
 		ws = /\s/,
 		i = this.length;
 	while (ws.test(this.charAt(--i)));
 	return str.slice(0, i + 1);
 };
+
+// jQuery extension to get the value, trim and return it (often useful for forms)
+(function($){
+  $.fn.trimval = function() {
+    return this.val().trimfast();
+  };
+})(jQuery);
